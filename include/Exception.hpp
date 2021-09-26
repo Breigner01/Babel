f242be12262844ed082bf642e275cc63fb486ada
Martin Olivier <martin.olivier@live.fr>
2021-09-27 01:47:41 +0200

    feat: babel::exception can be created with variadic template arguments

--- include/Exception.hpp
+++ include/Exception.hpp
@@ -2,15 +2,21 @@
 
 #include <exception>
 #include <string>
+#include <utility>
 
 namespace babel {
 
 class exception : public std::exception
 {
 private:
-    const std::string m_err;
+    std::string m_err;
 public:
     explicit exception(std::string err) : m_err(std::move(err)) {}
+    template<typename ...T>
+    exception(T &&...err)
+    {
+        m_err.append(std::forward<T>(err)...);
+    }
     ~exception() override = default;
     [[nodiscard]] const char *what() const noexcept override {return m_err.c_str();}
 };
