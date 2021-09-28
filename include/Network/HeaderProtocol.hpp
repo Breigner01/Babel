3e5fd300309e16c0c93e364764d5bdd1e3311a82
Coline <coline.seguret@epitech.eu>
2021-09-28 15:19:59 +0200

    feat: struct represents header protocol

---
+++ include/Network/HeaderProtocol.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace networkProtocol {
+    enum Type : uint8_t {
+        Error,
+        Song,
+        Video,
+        Text,
+        Reply
+    };
+
+    /**
+     *  Structure that represents a Header network protocol
+     */
+    struct HeaderProtocol {
+        uint32_t magicValue; // 0x42dead42
+        uint32_t id; // Chaque message a un identifiant. Une réponse ou une erreur aura le même identifiant que l'appel correspondant.
+        uint16_t version;
+        uint16_t type;
+        uint32_t payloadSize;
+        uint32_t action;
+        // ... 
+    };
+}
