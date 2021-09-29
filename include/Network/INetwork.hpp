c3cd147d955a620a4cfb37b85c2b8c3f3c077077
Coline <coline.seguret@epitech.eu>
2021-09-29 18:56:55 +0200

    feat: ASIO class which play server's role (protocol udp)

--- include/Network/INetwork.hpp
+++ include/Network/INetwork.hpp
@@ -1,22 +1,28 @@
 #pragma once
 
-#include <functional>
+#include <string>
 #include <vector>
+
+namespace Network {
+    template<typename T>
+    struct Client
+    {
+        asio::ip::udp::endpoint endpoint;
+        T buffer;
+    };
+}
 
 /**
  *  Interface that represents a network library
  */
+template<typename T, size_t L>
 class INetwork
 {
 public:
-    enum class protocol : bool {
-        udp,
-        tcp
-    };
-
     virtual ~INetwork() = default;
-    virtual void setTimeout(unsigned short timeout) noexcept = 0;
-    virtual void setBufferSize(unsigned short size) noexcept = 0;
-    virtual void send(const std::vector<unsigned char> &packet) = 0;
-    virtual std::vector<unsigned char> receive() = 0;
+    virtual void send(const Network::Client<T> &cli, const T &packet) = 0;
+    virtual void receive() = 0;
+    virtual void addClient(Network::Client<T> c) = 0;
+    virtual void removeClient(Network::Client<T> c) = 0;
+    virtual std::vector<Network::Client<T>> &getClients() noexcept = 0;
 };
