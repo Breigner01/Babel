e35556065106efd0311de196fc25e7309c63cffb
Martin Olivier <martin.olivier@live.fr>
2021-10-04 15:47:26 +0200

    fix: testmain

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main()
     std::unique_ptr<IEncoder<short, unsigned char>> op = std::make_unique<Opus<short>>();
     std::unique_ptr<INetwork<short, 2400>> socket = std::make_unique<ASIO<short, 2400>>(5000);
 
-    socket->addClient({asio::ip::udp::endpoint(asio::ip::make_address("10.41.174.229"), 5000), {}});
+    socket->addClient({asio::ip::udp::endpoint(asio::ip::make_address("10.41.154.16"), 5000), {}});
     std::cout << "Client Added" << std::endl;
 
     pa->getInputDevice()->start();
@@ -21,31 +21,28 @@ int main()
     std::cout << "Audio Started" << std::endl;
 
     while (true) {
-        std::cout << "while" << std::endl;
         // ENVOI
         auto in = pa->getInputDevice()->popBuffer();
 
         if (in.size() > 2400)
             in.resize(2400);
 
-        std::cout << in.size() << std::endl;
+        std::cout << "mic buf size : " << in.size() << std::endl;
         if (!in.empty()) {
             //auto packet = op->encode(in);
             socket->send(socket->getClients().front(), in);
-            std::cout << "send" << std::endl;
         }
         // RECEPTION
-        std::cout << "reicive1" << std::endl;
         socket->receive();
-        std::cout << "reicive2" << std::endl;
-        std::cout << "clients size : " << socket->getClients().size() << std::endl;
         for (auto &c : socket->getClients())
             std::cout << c.endpoint.address().to_string() << std::endl;
         auto &buf = socket->getClients().front().buffer;
         if (!buf.empty()) {
-            std::cout << "buffer" << std::endl;
-            for (const auto &i : buf)
+            for (const auto &i : buf) {
+                for (const auto &j : i)
+                    std::cout << j << std::endl;
                 pa->getOutputDevice()->pushBuffer(i);
+            }
         }
     }
 
@@ -56,3 +53,38 @@ int main()
 
     return EXIT_SUCCESS;
 }
+
+/*int main()
+{
+    std::string input;
+
+    std::unique_ptr<IAudio<short>> pa = std::make_unique<PortAudio<short>>();
+    std::unique_ptr<IEncoder<short, unsigned char>> op = std::make_unique<Opus<short>>();
+
+    std::cout << "Record Started" << std::endl;
+
+    pa->getInputDevice()->start();
+    std::chrono::system_clock::time_point deadline = std::chrono::system_clock::now() + std::chrono::seconds(5);
+
+    while (true) {
+        if (std::chrono::system_clock::now() >= deadline)
+            break;
+        auto in = pa->getInputDevice()->popBuffer();
+        if (!in.empty()) {
+            auto packet = op->encode(in);
+            pa->getOutputDevice()->pushBuffer(op->decode(packet)); 
+        }
+    }
+
+    std::cout << "Record Stoped" << std::endl;
+
+    pa->getInputDevice()->stop();
+    pa->getOutputDevice()->start();
+
+    std::cout << "Playing record" << std::endl;
+    std::getline(std::cin, input);
+
+    pa->getOutputDevice()->stop();
+
+    return EXIT_SUCCESS;
+}*/
