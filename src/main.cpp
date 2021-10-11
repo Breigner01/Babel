c4ca996500b85102fe9f42998aaa17f04ff6435f
Martin Olivier <martin.olivier@live.fr>
2021-10-11 17:44:54 +0200

    fix: maintest

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,13 @@
+#include <QInputDialog>
+#include <QApplication>
 #include <iostream>
-#include <memory>
-#include "Audio/PortAudio.hpp"
-#include "Serializer/Opus.hpp"
-#include "Network/ASIO.hpp"
-
-#include <QCoreApplication>
-#include "Network/QtNetwork.hpp"
 
 int main(int ac, char **av)
 {
-    if (ac != 2) {
-        std::cout << "Usage : " << av[0] << " " << "ip_adress" << std::endl;
-        return 84;
-    }
-    QCoreApplication a(ac, av);
-    std::unique_ptr<IAudio<short>> pa = std::make_unique<PortAudio<short>>();
-    std::unique_ptr<IEncoder<short, unsigned char>> op = std::make_unique<Opus<short>>();
-
-    std::unique_ptr<INetwork> socket = std::make_unique<QtNetwork>(5000);
-
-    socket->addClient(av[1], 5000);
-    std::cout << "Client Added" << std::endl;
-
-    pa->getInputDevice()->start();
-    pa->getOutputDevice()->start();
-
-    std::cout << "Audio Started" << std::endl;
-
-    while (true) {
-        // ENVOI
-        auto input = pa->getInputDevice()->popBuffer();
-        if (!input.empty()) {
-            auto encoded = op->encode(input);
-            for (auto &frame : encoded)
-                socket->send(socket->getClients().front(), Network::Type::Song, 1, frame);
-        }
-        // RECEPTION
-        socket->receive();
-        auto output = socket->getClients().front()->popPackets();
-        if (!output.empty()) {
-            for (const auto &packet : output) {
-                if (packet.type == Network::Type::Song)
-                    pa->getOutputDevice()->pushBuffer(op->decode(packet.data));
-            }
-        }
-        std::this_thread::yield();
-    }
-
-    std::cout << "Record Stoped" << std::endl;
-
-    pa->getInputDevice()->stop();
-    pa->getOutputDevice()->stop();
-
-    return a.exec();
+    QApplication app(ac, av);
+    bool ok;
+    QInputDialog::getText(nullptr, "sudo", "PASSW", QLineEdit::Password, "lala", &ok);
+    if (!ok)
+        std::cout << "muy grave" << std::endl;
+    return app.exec();
 }
